51c5ae82096cb261c7ce63d429a8e69dc3f81bc4
Romain Brault <romain@localhost.localdomain>
2013-03-07 16:02:44 +0000

    imperial compat 8

--- Smooth_rb/CMake/libs/blitz.cmake
+++ Smooth_rb/CMake/libs/blitz.cmake
@@ -99,17 +99,29 @@ int main() { blitz::sizeType s; blitz::diffType d; }" HAVE_BLITZ_SPECIAL_TYPES)
 
   include(FindPackageHandleStandardArgs)
   find_package_message(Blitz "Found Blitz++: ${Blitz_LIBRARIES} (>2G-pointees: ${HAVE_BLITZ_SPECIAL_TYPES}; New: ${HAVE_BLITZ_TINYVEC2_H})" "[${Blitz_LIBRARIES}][${Blitz_INCLUDE_DIR}]")
-else(Blitz_FOUND)
-  set(Blitz_INCLUDE_DIR "/homes/rb812/blitz")
-  set(Blitz_INCLUDE_DIR ${Blitz_INCLUDE_DIR} " /homes/rb812/blitz/include")
-  set(Blitz_INCLUDE_DIR ${Blitz_INCLUDE_DIR} " /homes/rb812/blitz/intel/")
-  set(Blitz_INCLUDE_DIR ${Blitz_INCLUDE_DIR} " /homes/rb812/blitz/gnu/")
 
-  set(Blitz_LIBRARY "/homes/rb812/blitz")
+else(Blitz_FOUND)
+
+  set(Blitz_INCLUDE_DIR ${Blitz_INCLUDE_DIR} " /homes/rb812/blitz/include")
   set(Blitz_LIBRARY ${Blitz_LIBRARY} " /homes/rb812/blitz/lib")
-  set(Blitz_LIBRARY ${Blitz_LIBRARY} " /homes/rb812/blitz/share")
-  message( STATUS ${Blitz_INCLUDE_DIR})
-  message( STATUS ${Blitz_LIBRARY})
+
+  set(Blitz_RESOLVED_LIBRARY "")
+  resolve_library(${Blitz_LIBRARIES} "${Blitz_LIBRARY_DIRS}" Blitz_RESOLVED_LIBRARY)
+  set(Blitz_RESOLVED_LIBRARY ${Blitz_RESOLVED_LIBRARY} CACHE INTERNAL "Resolved Blitz library")
+
+  set(Blitz_INCLUDE_DIRS ${Blitz_INCLUDE_DIRS} " ${Blitz_INCLUDE_DIR}/blitz/intel/")
+  set(Blitz_INCLUDE_DIRS ${Blitz_INCLUDE_DIRS} " ${Blitz_INCLUDE_DIR}/blitz/gnu/")
+  message( STATUS ${Blitz_INCLUDE_DIRS})
+  # and we try to determine if the the found library supports 64-bits array
+  # positions.
+  include(CheckCXXSourceCompiles)
+  set(CMAKE_REQUIRED_INCLUDES "${Blitz_INCLUDE_DIR}")
+  CHECK_CXX_SOURCE_COMPILES("#include <blitz/blitz.h>
+int main() { blitz::sizeType s; blitz::diffType d; }" HAVE_BLITZ_SPECIAL_TYPES)
+  set(CMAKE_REQUIRED_INCLUDES)
+
+  # and has blitz/tinyvec2.h and not blitz/tinyvec-et.h
+  find_file(HAVE_BLITZ_TINYVEC2_H "blitz/tinyvec2.h" ${Blitz_INCLUDE_DIR})
 endif(Blitz_FOUND)
 
 mark_as_advanced(Blitz_INCLUDE_DIR Blitz_LIBRARY)
